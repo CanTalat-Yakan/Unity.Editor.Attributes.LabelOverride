1b39043b92e87af9a045f0c5e1b6c89ee23beee7
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 12:50:09 +0200

    ⠀⠀⠀⠀⠀

--- Editor/LabelOverrideDrawer.cs
+++ Editor/LabelOverrideDrawer.cs
@@ -4,16 +4,29 @@ using UnityEditor;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Provides a custom property drawer for fields decorated with the <see cref="LabelOverrideAttribute"/>.
+    /// </summary>
+    /// <remarks>This drawer replaces the default label of a serialized property with the label specified in
+    /// the <see cref="LabelOverrideAttribute"/>. It does not support array elements and will log a warning if applied
+    /// to a property that is part of an array.</remarks>
     [CustomPropertyDrawer(typeof(LabelOverrideAttribute))]
     public class LabelOverrideDrawer : PropertyDrawer
     {
+        /// <summary>
+        /// Draws the custom GUI for a serialized property in the Unity Inspector, applying a label override if
+        /// specified.
+        /// </summary>
+        /// <remarks>If the property is not part of an array, the label is overridden with the value
+        /// specified in the <see cref="LabelOverrideAttribute"/>. If the property is part of an array, a warning is
+        /// logged, as label overrides are not supported for array elements.</remarks>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             try
             {
                 var propertyAttribute = this.attribute as LabelOverrideAttribute;
 
-                if (!IsPropertyAnArrayElement(property))
+                if (!InspectorHookUtilities.IsArrayElement(property))
                     label.text = propertyAttribute.Label;
                 else Debug.LogWarningFormat(
                     "{0}(\"{1}\") doesn't support arrays ",
@@ -23,18 +36,6 @@ namespace UnityEssentials
                 EditorGUI.PropertyField(position, property, label);
             }
             catch (System.Exception ex) { Debug.LogException(ex); }
-        }
-
-        private bool IsPropertyAnArrayElement(SerializedProperty property)
-        {
-            string path = property.propertyPath;
-
-            int dotIndex = path.IndexOf('.');
-            if (dotIndex == -1)
-                return false;
-
-            string propertyName = path.Substring(0, dotIndex);
-            return property.serializedObject.FindProperty(propertyName).isArray;
         }
     }
 }
--- Runtime/LabelOverrideAttribute.cs
+++ Runtime/LabelOverrideAttribute.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Specifies a custom label to display in the Unity Inspector for the associated property.
+    /// </summary>
+    /// <remarks>Use this attribute to override the default label of a property in the Unity Inspector. This
+    /// can be useful for providing more descriptive or user-friendly labels for fields.</remarks>
     public class LabelOverrideAttribute : PropertyAttribute
     {
         public string Label;
